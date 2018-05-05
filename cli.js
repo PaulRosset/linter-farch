2ab74b57533515caef14ae160c7aac1ae97e5fa3
Paul Rosset <paulrosset96@gmail.com>
2018-05-05 18:46:04 +0100

    Add new API, still cleaning to do

--- cli.js
+++ cli.js
@@ -19,30 +19,51 @@ const cli = meow(
 );
 
 const main = () => {
+  const reportFinal = {
+    errors: 0,
+    success: 0,
+    all: 0
+  };
   try {
     const data = fs.existsSync(path.resolve("farch.json"))
       ? loadJsonFile.sync(path.resolve("farch.json"))
       : loadJsonFile.sync(path.resolve("package.json"));
     const report = getInput(data);
-    console.log(`\n    Report ${chalk.bold("linter-farch:\n")}`);
+    console.log(`\n    ${chalk.bold("Report linter-farch:\n")}`);
 
     report.map((dir, index) => {
       const path = Object.keys(dir)[0];
       console.log(`    Directory: ${chalk.bold.blue(Object.keys(dir)[0])}`);
       dir[path].map(file => {
+        reportFinal.all += 1;
         if (!file.isCorrectSyntax) {
           console.log(
             `      ${chalk.underline(file.fileName)} - ${chalk.bold.red("●")}`
           );
-          assert.fail(`${file.fileName} doesn't match ${file.assertRegex}`);
-          process.exit(2);
+          console.log(
+            chalk.bold.red(
+              `        ${file.fileName} doesn't match /${file.assertRegex}/`
+            )
+          );
+          reportFinal.errors += 1;
         } else {
           console.log(
             `      ${chalk.underline(file.fileName)} - ${chalk.bold.green("✓")}`
           );
+          reportFinal.success += 1;
         }
       });
     });
+    console.log(
+      `\n${chalk.bold.white("Tested files:")} ${chalk.bold.white(
+        `${reportFinal.all} tests`
+      )}\n${chalk.bold.white("Tests passed:")} ${chalk.bold.green(
+        `${reportFinal.success} Passed`
+      )}\n${chalk.bold.white("Tests failed:")} ${chalk.bold.red(
+        `${reportFinal.errors} Failed`
+      )}`
+    );
+    reportFinal.errors > 0 ? process.exit(2) : process.exit(0);
   } catch (e) {
     if (e) {
       console.log(`      ${chalk.bold.red(e.message)}`);
