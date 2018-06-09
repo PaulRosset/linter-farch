a9ca9bdf9cb094cef6cb8013be1231f620d97bcd
Paul Rosset <paulrosset96@gmail.com>
2018-06-09 16:34:23 +0100

    Add testing

--- test/index.test.js
+++ test/index.test.js
@@ -21,95 +21,27 @@ const pkg1 = {
 };
 
 const pkg2 = {
-  name: "new",
-  version: "1.0.0",
-  description: "",
   farch: {
     "./src": "[a-z]*"
-  },
-  keywords: [],
-  main: "src/index.js",
-  dependencies: {
-    react: "16.3.2",
-    "react-dom": "16.3.2",
-    "react-scripts": "1.1.4"
-  },
-  devDependencies: {},
-  scripts: {
-    start: "react-scripts start",
-    build: "react-scripts build",
-    test: "react-scripts test --env=jsdom",
-    eject: "react-scripts eject"
   }
 };
 
 const pkg3 = {
-  name: "new",
-  version: "1.0.0",
-  description: "",
   farch: {
     "./media": "[0-9]"
-  },
-  keywords: [],
-  main: "src/index.js",
-  dependencies: {
-    react: "16.3.2",
-    "react-dom": "16.3.2",
-    "react-scripts": "1.1.4"
-  },
-  devDependencies: {},
-  scripts: {
-    start: "react-scripts start",
-    build: "react-scripts build",
-    test: "react-scripts test --env=jsdom",
-    eject: "react-scripts eject"
   }
 };
 
 const pkg4 = {
-  name: "new",
-  version: "1.0.0",
-  description: "",
   farch: {
     dir: "[0-9]"
-  },
-  keywords: [],
-  main: "src/index.js",
-  dependencies: {
-    react: "16.3.2",
-    "react-dom": "16.3.2",
-    "react-scripts": "1.1.4"
-  },
-  devDependencies: {},
-  scripts: {
-    start: "react-scripts start",
-    build: "react-scripts build",
-    test: "react-scripts test --env=jsdom",
-    eject: "react-scripts eject"
   }
 };
 
 const pkg5 = {
-  name: "new",
-  version: "1.0.0",
-  description: "",
   farch: {
     src: "[0-9]",
     "test/testFolder": "[0-9]"
-  },
-  keywords: [],
-  main: "src/index.js",
-  dependencies: {
-    react: "16.3.2",
-    "react-dom": "16.3.2",
-    "react-scripts": "1.1.4"
-  },
-  devDependencies: {},
-  scripts: {
-    start: "react-scripts start",
-    build: "react-scripts build",
-    test: "react-scripts test --env=jsdom",
-    eject: "react-scripts eject"
   }
 };
 
@@ -122,32 +54,40 @@ describe("Configuration testing", () => {
 });
 
 describe("Non Recursive testing", () => {
-  test("Expect a error throw, because dir did not exist", () => {
-    expect(() => {
-      const report = apiFarch(pkg4, { R: false });
-    }).toThrow();
+  test("Expect a error throw, because dir did not exist", done => {
+    Promise.all(apiFarch(pkg4, { R: false })).then(config => {
+      expect(config).toMatchSnapshot();
+      done();
+    });
   });
 
-  test("Expect to Match test directory filenames", () => {
-    const report = apiFarch(pkg2, { R: false });
-    expect(JSON.stringify(report)).toMatchSnapshot();
+  test("Expect to Match test directory filenames", done => {
+    Promise.all(apiFarch(pkg2, { R: false })).then(config => {
+      expect(JSON.stringify(config)).toMatchSnapshot();
+      done();
+    });
   });
 
-  test("Expect to NOT match filename in root diretory", () => {
-    const report = apiFarch(pkg3, { R: false });
-    expect(JSON.stringify(report)).toMatchSnapshot();
+  test("Expect to NOT match filename in root diretory", done => {
+    Promise.all(apiFarch(pkg3, { R: false })).then(config => {
+      expect(JSON.stringify(config)).toMatchSnapshot();
+      done();
+    });
+  });
+
+  test("Expect to match without recursive", done => {
+    Promise.all(apiFarch(pkg5, { R: false })).then(config => {
+      expect(JSON.stringify(config)).toMatchSnapshot();
+      done();
+    });
   });
 });
 
 describe("Recursive testing", () => {
-  test("Expect to output a strucutre with all files contained in directory given in input", () => {
-    const report = apiFarch(pkg5, { R: true });
-    expect(JSON.stringify(report)).toMatchSnapshot();
-  });
-
-  test("Will not work, cause undefined directory", () => {
-    expect(() => {
-      const report = apiFarch(pkg4, { R: false });
-    }).toThrow();
+  test("Expect to match with recursive", done => {
+    Promise.all(apiFarch(pkg5, { R: true })).then(config => {
+      expect(JSON.stringify(config)).toMatchSnapshot();
+      done();
+    });
   });
 });
