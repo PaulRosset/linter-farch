3aff7be1bc526545e3ef0e1594b60300fba5768f
Paul Rosset <paulrosset96@gmail.com>
2018-07-02 22:49:19 +0100

    Fix eslint on template file

--- src/template.js
+++ src/template.js
@@ -1,6 +1,6 @@
-// eslint no-use-before-define: 2
+/* eslint-disable */
 
 module.exports = {
-  LOWER_CAMEL_CASE_JS: "(^([a-z0-9]+?[A-Z]{1}[a-z0-9]*)+.js$)",
-  UPPER_CAMEL_CASE_JS: "(^((d)|([A-Z0-9][a-z0-9]+))*([A-Z])?.js$)"
+  LOWER_CAMEL_CASE_JS: "(^([a-z0-9]+?[A-Z]{1}[a-z0-9]*)+\.js$)",
+  UPPER_CAMEL_CASE_JS: "(^((d)|([A-Z0-9][a-z0-9]+))*([A-Z])?\.js$)"
 };
