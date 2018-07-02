139c0e2986bc1726cb9ac74741c5e539cf855ea1
Paul Rosset <paulrosset96@gmail.com>
2018-07-02 22:35:31 +0100

    Fix regex in template

--- src/template.js
+++ src/template.js
@@ -1,4 +1,4 @@
 module.exports = {
-  LOWER_CAMEL_CASE_JS: "(^([a-z0-9]+?[A-Z]{1}[a-z0-9]*)+.js$)",
-  UPPER_CAMEL_CASE_JS: "(^((d)|([A-Z0-9][a-z0-9]+))*([A-Z])?.js$)"
+  LOWER_CAMEL_CASE_JS: "(^([a-z0-9]+?[A-Z]{1}[a-z0-9]*)+\.js$)",
+  UPPER_CAMEL_CASE_JS: "(^((d)|([A-Z0-9][a-z0-9]+))*([A-Z])?\.js$)"
 };
